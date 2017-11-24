5e04afef4ced665fc14d895630785d4226e51b46
diogoremelgado <32169065+diogoremelgado@users.noreply.github.com>
2017-11-24 16:19:21 +0000

    Add files via upload

---
+++ olaamigossss.java
@@ -0,0 +1,28 @@
+package olaamigos;
+
+import java.util.Scanner;
+
+public class olaamigossss {
+
+
+    public static void main(String[] args) {
+        Scanner l = new Scanner(System.in);
+        int num; 
+        int fat = 1;
+        int cont = 1;
+
+        {
+            System.out.println("Digite um número: ");
+            num = l.nextInt();
+            
+            for(int I = 1;I <= num; I++){
+                fat = fat * I;
+            }
+            
+            System.out.println(" O fatorial é: " + fat);
+            cont++;
+            
+       }
+    }
+}
+    
