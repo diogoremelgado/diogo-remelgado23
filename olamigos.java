2e81a28eaffca5ba6edd559c6a25598640d007a8
diogoremelgado <32169065+diogoremelgado@users.noreply.github.com>
2017-11-24 14:59:58 +0000

    Add files via upload

---
+++ olamigos.java
@@ -0,0 +1,14 @@
+package olaamigos;
+
+
+
+public class olamigos {
+
+	public static void main(String[] args) {
+		String mensagem = "Hoje é Sexta-Feira!";
+		for (int I=1; I<=5; I++)
+			System.out.println(mensagem);
+
+	}
+
+}
