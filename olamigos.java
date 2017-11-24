27b3d5e8f32b1d41ba0888f01926aaecdce002d4
diogoremelgado <32169065+diogoremelgado@users.noreply.github.com>
2017-11-24 15:00:55 +0000

    Add files via upload

--- olamigos.java
+++ olamigos.java
@@ -6,7 +6,7 @@ public class olamigos {
 
 	public static void main(String[] args) {
 		String mensagem = "Hoje é Sexta-Feira!";
-		for (int I=1; I<=5; I++)
+		for (int I=1; I<=10; I++)
 			System.out.println(mensagem);
 
 	}
