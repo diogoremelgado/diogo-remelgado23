f64df6f96d77ffbae4798ca6ba075fb5860ae5d9
diogoremelgado <32169065+diogoremelgado@users.noreply.github.com>
2017-11-24 15:22:00 +0000

    Add files via upload

---
+++ olamigoss.java
@@ -0,0 +1,16 @@
+package olaamigos;
+
+public class olamigoss {
+
+	public static void main(String[] args) {
+	int soma=0;
+	
+	for(int I=1; I<=10; I++) {
+	soma +=I;
+	System.out.println(I);
+	}
+	System.out.println(soma);;		
+
+	}
+
+}
