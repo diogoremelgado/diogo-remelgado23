1b08ce17daafc277c41fcce2c2ba9c6a1a0b7ac8
diogoremelgado <32169065+diogoremelgado@users.noreply.github.com>
2017-11-24 15:51:29 +0000

    Add files via upload

--- olamigoss.java
+++ olamigoss.java
@@ -1,11 +1,14 @@
 package olaamigos;
-
+import java.util.Scanner;
 public class olamigoss {
 
 	public static void main(String[] args) {
+	Scanner LerS = new Scanner (System.in);
 	int soma=0;
-	
-	for(int I=1; I<=10; I++) {
+	int numero;
+	System.out.print("Digite o número:");
+	numero = LerS.nextInt();
+	for(int I=1; I<=numero; I++) {
 	soma +=I;
 	System.out.println(I);
 	}
